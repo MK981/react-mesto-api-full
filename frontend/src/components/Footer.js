445c5bdc2b6caaae040e89d61825d08d4f63d15b
Maxim Kuvaev <maks-kuvaev@mail.ru>
2021-10-10 21:11:32 +0300

    Улучшения 2(фикс)

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="footer">
-            <p className="footer__text">&copy; {new Date().getFullYear()} Mesto Russia</p>
+            <p className="footer__text">&copy; {new Date().getFullYear()} Maxim Kuvaev :)</p>
         </footer>
     );
 }
